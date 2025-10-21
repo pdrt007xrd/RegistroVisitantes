5b5519d93faf93cbb9f97e7a6525bb816bd8401e
Pedro <h4rds0ft@protonmail.com>
2025-10-21 00:03:36 -0400

    Solucionado problema de navegador movil del dt

--- obj/Debug/net8.0/WebApp.RegistroVisitantes.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp.RegistroVisitantes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp.RegistroVisitantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033e2d4b8d1e15931e283081ec7c2aa45ddd7848")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp.RegistroVisitantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp.RegistroVisitantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
